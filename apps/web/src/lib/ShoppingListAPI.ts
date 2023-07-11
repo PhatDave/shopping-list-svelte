29f5eec3d3cde80280be184daa302b5e0b99fed7
David Majdandžić <david.majdandzic@hotmail.com>
2023-07-11 16:17:00 +0200

    Reverse sorting order for shopping list

--- apps/web/src/lib/ShoppingListAPI.ts
+++ apps/web/src/lib/ShoppingListAPI.ts
@@ -3,7 +3,7 @@ import {env} from "$env/dynamic/public";
 
 class ShoppingListAPI extends API {
     url = `https://pocketbase-test.site.quack-lab.dev/api/collections/shopping_list/records`;
-    default_filter = '?perPage=10000&sort=-created';
+    default_filter = '?perPage=10000&sort=+created';
 }
 
 export const shoppingListApi = new ShoppingListAPI();
