30f4f8398f75bc5d83c16a1abb225d2be827c930
David Majdandžić <david.majdandzic@hotmail.com>
2023-09-06 23:35:52 +0200

    Migrate to new API

--- apps/web/src/lib/TodoListAPI.ts
+++ apps/web/src/lib/TodoListAPI.ts
@@ -1,8 +1,7 @@
 import API from "$lib/API";
-import {env} from "$env/dynamic/public";
 
 class TodoListAPI extends API {
-    url = `https://pocketbase-test.site.quack-lab.dev/api/collections/todo_list/records`;
+    url = `https://pocketbase-shoppinglist.site.cyka.info/api/collections/todo_list/records`;
     default_filter = '?perPage=10000&sort=-created';
 }
 
